d6e1a5f274ca68f6951baf255b1fcdc9128a514a
elijahchen <code.elijahchen@gmail.com>
2016-11-12 23:52:47 -0500

    - Completed set 13

--- src/b_13/A_of_13.java
+++ src/b_13/A_of_13.java
@@ -1,22 +1,31 @@
 package b_13;
 
+/*
+ * A- s = 1/2 + 1/3 + ... + 1 / n + 1 |ANS: 82
+ */
+
 public class A_of_13 {
-
-	private static void defineS(int num) {
-
-		int s = 0;
-		
-		
-			
-			
-			
-		}
-
-	}
 
 	public static void main(String[] args) {
 
-//		System.out.println(defineS(82));
+		double x = 2.0;
+		double n = 1.0;
+		double sum = 0;
+
+		while (n < 82) {
+			if (x > 4) {
+				double t = 1 / (n + 1);
+				sum = sum + t;
+				n++;
+			} else {
+				double t = 1 / x;
+				sum = sum + t;
+				x++;
+			}
+		}
+
+		System.out.println(sum);
+		System.out.println(n);
 
 	}
 
--- src/b_13/B_of_13.java
+++ src/b_13/B_of_13.java
@@ -1,22 +1,46 @@
 package b_13;
 
+import java.text.DecimalFormat;
+
+/*
+ *  S = sqrt( 6 * ( 1 + (1/2*2) + (1/3*3) + (1/4*4) + ... + (1/*n*n))
+ *  Evaluate S if n = 100
+ *  Calculate the value of n if s = PI to 6 decimal places
+ */
+
 public class B_of_13 {
-	
-	private static double evaluateS(int num){
-		
-		int s = 0;
-		
-		while(num >= 2){
-			
+
+	private static double evaluateS(double num) {
+
+		double s = 0;
+		double sum = 0;
+		double n = 2.0;
+
+		DecimalFormat thisFormat = new DecimalFormat("#.000000");
+
+		while (s != num) {
+			if (n == 2) {
+				sum += 1.0;
+			}
+			sum = sum + (1.0 / (n * n));
+
+			s = Math.sqrt(6 * sum);
+			s = Double.parseDouble((thisFormat.format(s)));
+			if (n == 100) {
+				System.out.println("If n = 100, S = " + s);
+			}
+			if (s != num) {
+				s = 0;
+			}
+			n++;
 		}
-		
-		return s;
+		return n;
 	}
-	
+
 	public static void main(String[] args) {
-		
-		System.out.println(evaluateS(100));
-		
+
+		System.out.println("If S = 3.141592, " + "n = " + (int) evaluateS(3.141592));
+
 	}
-	
+
 }
